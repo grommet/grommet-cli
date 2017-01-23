638d7fe64dd4749aff8160392960278505d39b69
Alan Souza <alansouzati@gmail.com>
2017-01-23 15:24:46 -0800

    v2.1.5: fixed eslint globals. Avoid using stateless functions as it does not work with hot reload.

--- templates/basic/src/js/App.js
+++ templates/basic/src/js/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import App from 'grommet/components/App';
 import Title from 'grommet/components/Title';
 
-export default () => (
-  <App>
-    <Title>Hello World</Title>
-    <p>Hello from a Grommet page!</p>
-  </App>
-);
+export default class BasicApp extends Component {
+  render() {
+    return (
+      <App>
+        <Title>Hello World</Title>
+        <p>Hello from a Grommet page!</p>
+      </App>
+    );
+  }
+}
